c6b9fd7b131f293ee5231d9357083fe820685d6a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-12-08 15:19:45 +0900

    Patch test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-
 set -eu
 
-./scripts/build.sh
-npx jest $@
+# Install wasm-pack
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+# Clone swc
+git clone https://github.com/swc-project/swc.git -b swc-win-arm64
+cd swc
+
+# Install dependencies
+yarn
+
+# Test wasm
+(cd ./bindings/binding_typescript_wasm && ./scripts/build.sh && ./scripts/test.sh && node nodejs-patched.mjs)
